9f7268a84ee0c7e7c01f028f2e084004eecf6d9c
Bgguy154 <144231038+Bgguy154@users.noreply.github.com>
2025-11-19 14:47:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+const fname=document.querySelector("input[name=fname]");
+const lname=document.queryselector("input[name=lname]");
+const submitBtn=document.querySelector("input[type=submit]");
+submitBtn.addEventListener('click',(e)=>{
+	e.preventdefault();
+	getFormvalue();
+})
 function getFormvalue() {
     //Write your code here
-
+    alert(${fname},${lname});
 }
+
